0aebe017f0a1e1fed91319b023308c364326f7a7
yangzhen <1784054000@qq.com>
2017-03-07 18:35:03 +0800

    修改支付方法返回值

--- doExtLib/do_AppleIAP_SM.m
+++ doExtLib/do_AppleIAP_SM.m
@@ -114,9 +114,6 @@
             [self verifyPurchaseWithPaymentTransaction];
             //结束支付交易
             [queue finishTransaction:paymentTransaction];
-            doInvokeResult *_invokeResult = [[doInvokeResult alloc] init];
-            [_invokeResult SetResultBoolean:YES];
-            [_scritEngine Callback:_callbackName :_invokeResult];
         }
         else if(paymentTransaction.transactionState == SKPaymentTransactionStateRestored)
         {//恢复成功，对于非消耗品才能恢复,如果恢复成功则transaction中记录的恢复的产品交易
@@ -132,9 +129,9 @@
             if (paymentTransaction.error.code == SKErrorPaymentCancelled)
             {//如果用户点击取消
                 NSLog(@"取消购买.");
-                doInvokeResult *_invokeResult = [[doInvokeResult alloc] init];
-                [_invokeResult SetResultBoolean:NO];
-                [_scritEngine Callback:_callbackName :_invokeResult];
+//                doInvokeResult *_invokeResult = [[doInvokeResult alloc] init];
+//                [_invokeResult SetResultBoolean:NO];
+//                [_scritEngine Callback:_callbackName :_invokeResult];
             }
         }
         
@@ -213,50 +210,61 @@
     NSData *receiptData = [NSData dataWithContentsOfURL:receiptUrl];
     
     NSString *receiptString = [receiptData base64EncodedStringWithOptions:NSDataBase64EncodingEndLineWithLineFeed];//转化为base64字符串
+    NSMutableDictionary *node = [NSMutableDictionary dictionary];
+    [node setObject:receiptString forKey:@"receiptData"];
+    doInvokeResult *invoke = [[doInvokeResult alloc]init:self.UniqueKey];
+    [invoke SetResultNode:node];
+    [_scritEngine Callback:_callbackName :invoke];
     
-    NSString *bodyString = [NSString stringWithFormat:@"{\"receipt-data\" : \"%@\"}", receiptString];//拼接请求数据
-    NSData *bodyData = [bodyString dataUsingEncoding:NSUTF8StringEncoding];
-    
-    //创建请求到苹果官方进行购买验证
-    NSURL *url=[NSURL URLWithString: self.appStoreVerifyURL];
-    NSMutableURLRequest *requestM = [NSMutableURLRequest requestWithURL:url];
-    requestM.HTTPBody = bodyData;
-    requestM.HTTPMethod = @"POST";
-    //创建连接并发送同步请求
-    NSError *error = nil;
-    NSData *responseData = [NSURLConnection sendSynchronousRequest:requestM returningResponse:nil error:&error];
-    if (error)
-    {
-        NSLog(@"验证购买过程中发生错误，错误信息：%@",error.localizedDescription);
-        return;
-    }
-    NSDictionary *dic = [NSJSONSerialization JSONObjectWithData:responseData options:NSJSONReadingAllowFragments error:nil];
-    NSLog(@"%@",dic);
-    if([dic[@"status"] intValue] == 0)
-    {
-        NSLog(@"购买成功！");
-        NSDictionary *dicReceipt = dic[@"receipt"];
-        NSDictionary *dicInApp = [dicReceipt[@"in_app"] firstObject];
-        NSString *productIdentifier = dicInApp[@"product_id"];//读取产品标识
-        //如果是消耗品则记录购买数量，非消耗品则记录是否购买过
-        NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
-        if ([productIdentifier isEqualToString:self.productID])
-        {
-            NSInteger purchasedCount = [defaults integerForKey:productIdentifier];//已购买数量
-            [defaults setInteger:(purchasedCount+1) forKey:productIdentifier];
-        }
-        else
-        {
-            [defaults setBool:YES forKey:productIdentifier];
-        }
-        [defaults synchronize];
-
-        //在此处对购买记录进行存储，可以存储到开发商的服务器端
-    }
-    else
-    {
-        NSLog(@"购买失败，未通过验证！");
-    }
+    return;
+//    NSData *bodyData = [bodyString dataUsingEncoding:NSUTF8StringEncoding];
+//    
+//    //创建请求到苹果官方进行购买验证
+//    NSURL *url=[NSURL URLWithString: self.appStoreVerifyURL];
+//    NSMutableURLRequest *requestM = [NSMutableURLRequest requestWithURL:url];
+//    requestM.HTTPBody = bodyData;
+//    requestM.HTTPMethod = @"POST";
+//    //创建连接并发送同步请求
+//    NSError *error = nil;
+//    NSData *responseData = [NSURLConnection sendSynchronousRequest:requestM returningResponse:nil error:&error];
+//    if (error)
+//    {
+//        NSLog(@"验证购买过程中发生错误，错误信息：%@",error.localizedDescription);
+//        return;
+//    }
+//    NSDictionary *dic = [NSJSONSerialization JSONObjectWithData:responseData options:NSJSONReadingAllowFragments error:nil];
+//    NSLog(@"%@",dic);
+//    if([dic[@"status"] intValue] == 0 && dic)
+//    {
+//        NSLog(@"购买成功！");
+//        doInvokeResult *_invokeResult = [[doInvokeResult alloc] init];
+//        [_invokeResult SetResultText:receiptString];
+//        [_scritEngine Callback:_callbackName :_invokeResult];
+//        NSDictionary *dicReceipt = dic[@"receipt"];
+//        NSDictionary *dicInApp = [dicReceipt[@"in_app"] firstObject];
+//        NSString *productIdentifier = dicInApp[@"product_id"];//读取产品标识
+//        //如果是消耗品则记录购买数量，非消耗品则记录是否购买过
+//        NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
+//        if ([productIdentifier isEqualToString:self.productID])
+//        {
+//            NSInteger purchasedCount = [defaults integerForKey:productIdentifier];//已购买数量
+//            [defaults setInteger:(purchasedCount+1) forKey:productIdentifier];
+//        }
+//        else
+//        {
+//            [defaults setBool:YES forKey:productIdentifier];
+//        }
+//        [defaults synchronize];
+//
+//        //在此处对购买记录进行存储，可以存储到开发商的服务器端
+//    }
+//    else
+//    {
+//        doInvokeResult *_invokeResult = [[doInvokeResult alloc] init];
+//        [_invokeResult SetResultText:@""];
+//        [_scritEngine Callback:_callbackName :_invokeResult];
+//        NSLog(@"购买失败，未通过验证！");
+//    }
 }
 
 @end
